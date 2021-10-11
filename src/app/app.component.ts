45e36926e4a20c1447d0fcf37b3709e1e89fe981
diwen <devinz5050@outlook.com>
2021-10-11 19:53:40 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
+/* Default component for Angular
+AUTHOR: Diwen Zhang
+EMAIL:  zhangdiw@sheridancollege.ca
+CREATED: 2021-08-13
+UPDATED: 2021-08-14 
+*/
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Assignment4-DiwenZhang';
+  videos:any;
+  //inject httpclient service to load local json
+  constructor(http:HttpClient){
+    const URL = "assets/video_diwenzhang.json";
+    http.get(URL).subscribe(json => {
+      this.videos = json;
+    },error => {
+      console.log(error.message);
+    });
+  }
+
+  openVideo(index:number){
+    let url = this.videos[index].url;
+    window.open(url,"_blank");
+  }
+
+  getAvatarUrl(index:number){
+    let url:string;
+    url = "url('"+this.videos[index].avatarUrl+"')";
+    return url;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+/* Default Module for Angular
+AUTHOR: Diwen Zhang
+EMAIL:  zhangdiw@sheridancollege.ca
+CREATED: 2021-08-13
+UPDATED: 2021-08-14 
+*/
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpClientModule, BrowserAnimationsModule,MatCardModule,MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
